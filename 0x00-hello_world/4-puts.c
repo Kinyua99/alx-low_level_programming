e9298c24ea8a259540b7ef1919f8bf72edcbb825
Kinyua99 <kinyuangumba@01.com>
2022-12-08 03:09:06 -0800

    Almost

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -16,8 +16,8 @@ int main(void)
 
 {
 
-		puts("Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle\n");
 
-			return (0);
+		return (0);
 
 }
