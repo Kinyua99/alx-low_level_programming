15b4a070dee30ee78ee89dbd17858eb8fec92dcd
Kinyua99 <kinyuangumba@01.com>
2022-12-08 03:06:15 -0800

    Execute

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,3 @@
-#!/bin/bash
 #include <stdio.h>
 
 
@@ -17,7 +16,7 @@ int main(void)
 
 {
 
-		puts("\"Programming is like building a multilingual puzzle");
+		puts("Programming is like building a multilingual puzzle\n");
 
 			return (0);
 
