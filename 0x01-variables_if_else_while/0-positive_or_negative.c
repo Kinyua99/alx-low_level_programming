46c2075efbc0d7fd152158ce41af61ac897328b0
Kinyua99 <kinyuangumba@01.com>
2022-12-09 13:38:14 -0800

    s

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -13,10 +13,10 @@
 
 
 /**
- * main() - Prints a random number and states whether it is positive,
- * negative, or zero.
+ * main - Entry point
  *
- * Return: Always 0.
+ * Return: Always 0 (Success)
+ *
  */
 
 int main(void)
