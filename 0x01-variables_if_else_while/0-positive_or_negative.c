484fad0bfbd9f636b298153fbc8f744bdeba28b8
Kinyua99 <kinyuangumba@01.com>
2022-12-09 13:31:06 -0800

    a

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -13,7 +13,8 @@
 
 
 /**
- * main() - Prints a random number and states whether it is positive, negative, or zero.
+ * main() - Prints a random number and states whether it is positive,
+ * negative, or zero.
  *
  * Return: Always 0.
  */
