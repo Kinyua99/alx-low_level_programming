182ca53774587509a03ae84b76fe18d3f496d8bb
Kinyua99 <kinyuangumba@01.com>
2022-12-14 11:10:11 -0800

    AlphaX10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,17 +1,23 @@
 #include "main.h"
 
+/**
+ * print_alphabet_x10 - Make alphabet x10 times
+ *
+ * Return: void
+ */
+
 void print_alphabet_x10(void)
 {
 	char k;
 	int ctr = 0;
-	while (ctr <=0)
+
+	while (ctr <= 9)
 	{
-		for (k='a'; k<='z'; k++)
+		for (k = 'a'; k <= 'z'; k++)
 		{
 			_putchar(k);
 		}
 		_putchar('\n');
-		
-		ctr ++;
+		ctr++;
 	}
 }
