616d8a3b6788557fc23ab65470df63580643c8fa
Kinyua99 <kinyuangumba@01.com>
2022-12-16 12:18:15 -0800

    is it post or neg

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -2,14 +2,15 @@
 
 /**
  * main - tests function that prints if positive or negative
- *
  * Return: 0
  */
 
 int main(void)
 {
 	int i;
-	i = 0;
+
+	i = 98;
 	positive_or_negative(i);
+
 	return (0);
 }
