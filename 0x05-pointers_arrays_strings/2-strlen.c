d35e2b824aa480d8406e89430ca78306761e0847
Kinyua99 <kinyuangumba@01.com>
2022-12-19 08:47:26 -0800

    String length

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strlen- returns the length of the string
+ * @s: input string
+ * Return: returns the string length
+ */
+
+int _strlen(char *s)
+{
+	int cnt = 0;
+	while (*(s + cnt) != '\0')
+	{
+		cnt++;
+	}
+	return (cnt);
+}
