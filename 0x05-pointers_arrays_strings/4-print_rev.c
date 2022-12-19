a9af16088287e73cc0ba95e21cc58a8514fb3f5a
Kinyua99 <kinyuangumba@01.com>
2022-12-19 09:16:24 -0800

    Reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _print_rev - prints a string in reverse, followed by a new line
+ * @s: input string
+ * Return: no return
+ */
+
+void print_rev(char *s)
+{
+	int cn = 0;
+
+	while (cnt >= 0)
+	{
+		if (s[count] == '\0')
+			break;
+		cnt++;
+	}
+	for (cnt--; cnt >= 0; cnt--)
+		_putchar(s[cnt]);
+	_putchar('\n');
+}
