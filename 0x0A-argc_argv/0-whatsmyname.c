d0b94bf85403bb0656de7092dee582af29adb0fa
Kinyua99 <kinyuangumba@01.com>
2023-01-09 07:23:54 -0800

    Prints function name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - prints name of function followed by new line
+ * @argc: number of command line arguments
+ * @argv: array of program command line arguments
+ * Return: 0 _ success
+ */
+int main(int argc, char *argv[])
+{
+	ptintf("%s\n", argv[0]);
+	return (0);
+}
