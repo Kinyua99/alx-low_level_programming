4df5128010221d75e44416f8ae455c329f90bb6c
Kinyua99 <kinyuangumba@01.com>
2023-01-09 07:26:47 -0800

    Prints function name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -6,7 +6,7 @@
  * @argv: array of program command line arguments
  * Return: 0 _ success
  */
-int main(int argc, char *argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
 	ptintf("%s\n", argv[0]);
 	return (0);
