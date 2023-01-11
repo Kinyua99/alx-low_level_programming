fb56072b427ec7b0ddc7e211502242e170e4bf99
Kinyua99 <kinyuangumba@01.com>
2023-01-11 11:53:22 -0800

    Grid

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -13,15 +13,23 @@ int **alloc_grid(int width, int height)
 	int **gridout;
 	int i, j;
 
-	if (width < 1 || height < 1)
+	if (width <= 0 || height <= 0)
 		return (NULL);
 
 	gridout = malloc(height * sizeof(int *));
 
 	if (gridout == NULL)
-	{
-		free(gridout);
 		return (NULL);
+	for (i = 0; i < height; i++)
+	{
+		gridout[i] = malloc(sizeof(int) * width);
+		if (gridout[i] == NULL)
+		{
+			for (; i >= 0; i++)
+				free(gridout[i]);
+			free(gridout);
+			return (NULL);
+		}
 	}
 	for (i = 0; i < height; i++)
 		for (j = 0; j < width; j++)
