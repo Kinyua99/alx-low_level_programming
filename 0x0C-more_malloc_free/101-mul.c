25ae961e07921447540bf4fbf1d1043652a6b0f1
Kinyua99 <kinyuangumba@01.com>
2023-01-13 13:32:47 -0800

    Multiply

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -31,8 +31,8 @@ void _is_zero(char *argv[])
 	}
 }
 /**
- * _initialize_array - set memory to 0 in new array
- * @arr: char array
+ * _initialize_array - set memory to zero in new array
+ * @ar: char array
  * @lar: array length
  * Return: pointer to array
  */
@@ -46,7 +46,7 @@ char *_initialize_array(char *ar, int lar)
 	return (ar);
 }
 /**
- * _check_num - determines length pof number
+ * _checknum - determines length pof number
  * and checks if number is in base 10
  * @argv: argument vector
  * @n: array rows
@@ -78,18 +78,15 @@ int main(int argc, char *argv[])
 
 	if (argc != 3)
 		printf("Error\n"), exit(98);
-
 	ln1 = _checknum(argv, 1), ln2 = _checknum(argv, 2);
 	_is_zero(argv), lnout = ln1 + ln2, nout = malloc(lnout + 1);
 
 	if (nout == NULL)
 		printf("Error\n"), exit(98);
-
 	nout = _initialize_array(nout, lnout);
 	k = lnout - 1, i = ln1 - 1, j = ln2 - 1, ca = addl = 0;
 
 	for (; k >= 0; k--, i--)
-	{
 		if (i < 0)
 		{
 			if (addl > 0)
@@ -109,13 +106,11 @@ int main(int argc, char *argv[])
 			free(nout), nout = malloc(lnout + 1), nout = _initialize_array(nout, lnout);
 			k = lnout - 1, i = ln1 - 1, j = ln2 - 1, ca = addl = 0;
 		}
-
 		if (j >= 0)
 		{
 			add = ((argv[1][i] - '0') * (argv[2][j] - '0')) + (nout[k] - '0') + addl;
 			addl = add / 10, nout[k] = (add % 10) + '0';
 		}
-	}
 	printf("%s\n", nout);
 	return (0);
 }
