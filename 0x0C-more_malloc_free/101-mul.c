685f867234da68ebcbed485709abe44ae1e513dc
Kinyua99 <kinyuangumba@01.com>
2023-01-13 13:58:30 -0800

    Multiply

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -80,13 +80,12 @@ int main(int argc, char *argv[])
 		printf("Error\n"), exit(98);
 	ln1 = _checknum(argv, 1), ln2 = _checknum(argv, 2);
 	_is_zero(argv), lnout = ln1 + ln2, nout = malloc(lnout + 1);
-
 	if (nout == NULL)
 		printf("Error\n"), exit(98);
 	nout = _initialize_array(nout, lnout);
 	k = lnout - 1, i = ln1 - 1, j = ln2 - 1, ca = addl = 0;
-
 	for (; k >= 0; k--, i--)
+	{
 		if (i < 0)
 		{
 			if (addl > 0)
@@ -111,6 +110,7 @@ int main(int argc, char *argv[])
 			add = ((argv[1][i] - '0') * (argv[2][j] - '0')) + (nout[k] - '0') + addl;
 			addl = add / 10, nout[k] = (add % 10) + '0';
 		}
+	}
 	printf("%s\n", nout);
 	return (0);
 }
