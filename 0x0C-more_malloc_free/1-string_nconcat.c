e3b8f7f606d232f9dd834e3462f5e52e646ad62d
Kinyua99 <kinyuangumba@01.com>
2023-01-13 06:23:27 -0800

    concatenate

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -3,46 +3,39 @@
 
 /**
  * string_nconcat - concatenates 2 strings
- * @s1: string 1
- * @s2: string 2
- * @n: siz
- * Return: pointer to newly allocated memory, otherwise NULL
+ * @s1:  string 1
+ * @s2: string2
+ * @n: amount of bytes
+ * Return: pointer to allocated memory, otherwise nNULL
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int i = 0, j = 0, k = 0, l = 0;
-	char *str;
+	char *sout;
+	unsigned int ls1, ls2, lsout, i;
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-	while (s1[1])
-		i++;
-	while (s2[k])
-		k++;
-	if (n >= k)
-		l = k + i;
-	else
-		l = n + i;
+	for (ls1 = 0; s1[ls1] != '\0'; ls1++)
+		;
+	for (ls2 = 0; s2[ls2] != '\0'; ls2++)
+		;
+	if (n > ls2)
+		n = ls2;
 
-	str = malloc(sizeof(char) * l + 1);
-	if (str == NULL)
+	lsout = ls1 + n;
+	sout = malloc(lsout + 1);
+
+	if (sout == NULL)
 		return (NULL);
-
-	k = 0;
-
-	while (j < l)
+	for (i = 0; i < lsout; i++)
 	{
-		if (j <= i)
-			str[j] = s1[j];
-		if (j >= i)
-		{
-			str[j] = s2[k];
-			k++;
-		}
-		j++;
+		if (i < ls1)
+			sout[i] = s1[i];
+		else
+			sout[i] = s2[i - ls1];
 	}
-	str[j] = '\0';
-	return (str);
+	sout[i] = '\0';
+	return (sout);
 }
