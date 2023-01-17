333209c985bdcc29385f728813448d5160205195
Kinyua99 <kinyuangumba@01.com>
2023-01-17 11:18:21 -0800

    Initializing struct

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,20 @@
+#include "dog.h"
+
+/**
+ * main - initializes struct dog
+ * @ name: dog name
+ * @age: dog age
+ * @owner: dog owner
+ * @d: struct dog pointer
+ * Return 0 on success
+ */
+int main(void)
+{
+	if (d)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
+	return (0);
+}
