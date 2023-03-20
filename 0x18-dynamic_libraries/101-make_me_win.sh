ad3b258858adc869a514fdf2854572d6dd1cce37
Kinyua99 <kinyuangumba@01.com>
2023-03-20 12:42:00 -0700

    Edit

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp/ https://raw.github.com/Kinyua99/holbertonschool-low_level_programming/master/0x18-dynamic_libraries/libtest.so
-export LD_PRELOAD=/$PWD/libtest.so
+wget -P /tmp/ https://raw.github.com/Kinyua99/alx-low_level_programming/master/0x18-dynamic_libraries/libtest.so
+export LD_PRELOAD=/tmp/libtest.so
